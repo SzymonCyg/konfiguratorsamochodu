fb0b89ddefb908bb3f97d272f084623af83acb8e
szymon <sda@sda/pl>
2025-03-14 14:26:07 +0100

    dodanie checkboxow przycisku i radiobuttonow

--- app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
+++ app/src/main/java/com/example/konfiguratorsamochodu/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.konfiguratorsamochodu
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,53 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val myTextView=findViewById<TextView>(R.id.podsumowanie)
+
+        val Podgrzewane_siedzenia:CheckBox = findViewById(R.id.podgrzewane_siedzenia)
+        Podgrzewane_siedzenia.setOnCheckedChangeListener{ _, isChecked->
+            if(isChecked){
+                myTextView.text="Podgrzewane siedzenia"
+            }
+            else{
+
+            }
+        }
+        val Klimatyzacja: CheckBox = findViewById(R.id.klimatyzacja)
+        Klimatyzacja.setOnCheckedChangeListener{ _, isChecked->
+            if(isChecked){
+                myTextView.text="Klimatyzacja"
+            }
+            else{
+
+            }
+        }
+        val Szyberdach: CheckBox = findViewById(R.id.szyberdach)
+        Szyberdach.setOnCheckedChangeListener{ _, isChecked->
+            if(isChecked){
+                myTextView.text="Szyberdach"
+            }
+            else{
+
+            }
+        }
+        val Skorzana_tapicerka: CheckBox = findViewById(R.id.skorzana_tapicerka)
+        Skorzana_tapicerka.setOnCheckedChangeListener{ _, isChecked->
+            if(isChecked){
+                myTextView.text="Skorzana_tapicerka"
+            }
+            else{
+
+            }
+        }
+        val Automatyczna_skrzynia: CheckBox = findViewById(R.id.automatyczna_skrzynia)
+        Automatyczna_skrzynia.setOnCheckedChangeListener{ _, isChecked->
+            if(isChecked){
+                myTextView.text="Automatyczna_skrzynia"
+            }
+            else{
+
+            }
+        }
+
     }
 }
